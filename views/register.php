bccda97e6ecc6007190538b8c6f66ec0455a2e9d
Philippe Cossette <philcossette@hotmail.com>
2025-08-12 12:10:45 -0400

    Finalisation du form pour update

--- views/register.php
+++ views/register.php
@@ -1,33 +1,52 @@
-{{ include('layouts/header.php', {title:'Page d\'inscription - Stampee'})}}
+{{ include("layouts/header.php", { title: "Page d'inscription - Stampee" }) }}
 <div class="wrapper-centered">
-
-<form class="form" action="{{base}}/register" method="post">
-  <header class="form-header">
-  <p class="form-header-undertitle">Entrez vos informations</p>
-  <h1 class="form-header-title">Créez votre compte</h1>
-</header>
-  <div>
-  <input class="form-input" type="text" id="nom_utilisateur" name="nom_utilisateur" placeholder="Nom d'utilisateur" required>
-  {% if errors.nom_utilisateur is defined %}                   
-    <span class="error">{{ errors.nom_utilisateur }}</span>
-  {% endif %}
-  </div>
-  <div>
-    <input class="form-input" type="email" id="email" name="email" placeholder="Adresse Email" required>
-    {% if errors.email is defined %}
+  <form class="form" action="{{ base }}/register" method="post">
+    <header class="form-header">
+      <p class="form-header-undertitle">Entrez vos informations</p>
+      <h1 class="form-header-title">Créez votre compte</h1>
+    </header>
+    <div>
+      <input
+        class="form-input"
+        type="text"
+        id="nom_utilisateur"
+        name="nom_utilisateur"
+        placeholder="Nom d'utilisateur"
+        required
+      />
+      {% if errors.nom_utilisateur is defined %}
+      <span class="error">{{ errors.nom_utilisateur }}</span>
+      {% endif %}
+    </div>
+    <div>
+      <input
+        class="form-input"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Adresse Email"
+        required
+      />
+      {% if errors.email is defined %}
       <span class="error">{{ errors.email }}</span>
-    {% endif %}
-  </div>
-  <div>
-    <input class="form-input" type="password" id="mot_de_passe" name="mot_de_passe" placeholder="Mot de Passe" required>
-    {% if errors.mot_de_passe is defined %}
+      {% endif %}
+    </div>
+    <div>
+      <input
+        class="form-input"
+        type="password"
+        id="mot_de_passe"
+        name="mot_de_passe"
+        placeholder="Mot de Passe"
+        required
+      />
+      {% if errors.mot_de_passe is defined %}
       <span class="error">{{ errors.mot_de_passe }}</span>
-    {% endif %}
-  </div>
+      {% endif %}
+    </div>
 
-  <button class="main-button" type="submit">S'inscrire</button>
-</form>
+    <button class="main-button" type="submit">S'inscrire</button>
+  </form>
 </div>
 
-
-{{ include('layouts/footer.php')}}
+{{ include("layouts/footer.php") }}
--- views/update.php
+++ views/update.php
@@ -1,26 +1,53 @@
-{{ include('layouts/header.php', {title:'Modifer Profile - Stampee'})}}
+{{ include("layouts/header.php", { title: "Modifer Profile - Stampee" }) }}
 
-
-<form class="form" action="{{base}}/user/update" method="post">
-<input type="text" name="nom_utilisateur" id="nom_utilisateur" value="{{ inputs.nom_utilisateur }}" class="form-input" required>
-{% if errors.nom_utilisateur is defined %}
+<div class="wrapper-centered">
+  <form class="form" action="{{ base }}/user/update" method="post">
+    <header class="form-header">
+      <p class="form-header-undertitle">Changez vos informations</p>
+      <h1 class="form-header-title">Modifier mon profil</h1>
+    </header>
+    <div></div>
+    <div>
+      <input
+        type="text"
+        name="nom_utilisateur"
+        id="nom_utilisateur"
+        value="{{ inputs.nom_utilisateur }}"
+        class="form-input"
+        required
+      />
+      {% if errors.nom_utilisateur is defined %}
       <span class="error">{{ errors.nom_utilisateur }}</span>
-    {% endif %}
-<input type="email" name="email" id="email" value="{{ inputs.email}}" class="form-input" required>
-{% if errors.email is defined %}
+      {% endif %}
+    </div>
+    <div>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        value="{{ inputs.email }}"
+        class="form-input"
+        required
+      />
+      {% if errors.email is defined %}
       <span class="error">{{ errors.email }}</span>
-    {% endif %}
-<input type="password" name="mot_de_passe" id="mot_de_passe" value="" class="form-input" placeholder="Mot de Passe">
-{% if errors.mot_de_passe is defined %}
+      {% endif %}
+    </div>
+    <div>
+      <input
+        type="password"
+        name="mot_de_passe"
+        id="mot_de_passe"
+        value=""
+        class="form-input"
+        placeholder="Mot de Passe"
+      />
+      {% if errors.mot_de_passe is defined %}
       <span class="error">{{ errors.mot_de_passe }}</span>
-    {% endif %}
-<button type='submit'>Mettre a jour</button>
-</form>
+      {% endif %}
+    </div>
+    <button class="main-button" type="submit">Mettre a jour</button>
+  </form>
+</div>
 
-
-
-
-
-
-
-{{ include('layouts/footer.php')}}
+{{ include("layouts/footer.php") }}
