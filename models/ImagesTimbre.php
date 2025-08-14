3a0d79970c3861e4f49d22b104274eb2573d5ca5
Philippe Cossette <philcossette@hotmail.com>
2025-08-14 14:01:10 -0400

    Added method in ImagesTimbre model to select all images with the given timbre_id

--- models/ImagesTimbre.php
+++ models/ImagesTimbre.php
@@ -9,4 +9,13 @@ class ImagesTimbre extends CRUD
     protected $table = 'images_timbre';
     protected $primaryKey = 'id';
     protected $fillable = ['id_timbre', 'url_image', 'principale'];
+
+    public function selectByTimbre($timbreId)
+    {
+        $sql = "SELECT * FROM $this->table WHERE id_timbre = :id_timbre";
+        $stmt = $this->prepare($sql);
+        $stmt->bindValue(":id_timbre", $timbreId);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 }
