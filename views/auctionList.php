fef5ef144a827111e5a7344300959efabe6caca0
Philippe Cossette <philcossette@hotmail.com>
2025-08-19 10:14:14 -0400

    Created A js function to display time left on the auction

--- views/auctionList.php
+++ views/auctionList.php
@@ -1,3 +1,5 @@
+{{ include("layouts/header.php", { title: "Page du Profile - Stampee" }) }}
+
 <h1>Liste des enchères</h1>
 
 {% for enchere in encheres %}
@@ -5,7 +7,8 @@
     <h2>{{ enchere.titre }}</h2>
     <img src="{{asset}}uploads/{{ enchere.image_principale }}" alt="{{ enchere.titre }}">
     <p><strong>Prix départ :</strong> {{ enchere.prix_actuel }} $</p>
-    <p><strong>Début :</strong> {{ enchere.debut }}</p>
-    <p><strong>Fin :</strong> {{ enchere.fin }}</p>
+    <p>Temps restant : <span class="timer" data-fin="{{ enchere.fin }}"></span></p>
 </div>
 {% endfor %}
+
+{{ include("layouts/footer.php") }}
