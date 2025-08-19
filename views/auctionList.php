0d50905876e5bfc1d5bb008d3ae3efa94227adda
Philippe Cossette <philcossette@hotmail.com>
2025-08-19 10:23:55 -0400

    Created auction list page layout and added id in the href for a auction page

--- views/auctionList.php
+++ views/auctionList.php
@@ -2,13 +2,20 @@
 
 <h1>Liste des enchères</h1>
 
-{% for enchere in encheres %}
-<div class="enchere-card">
-    <h2>{{ enchere.titre }}</h2>
-    <img src="{{asset}}uploads/{{ enchere.image_principale }}" alt="{{ enchere.titre }}">
-    <p><strong>Prix départ :</strong> {{ enchere.prix_actuel }} $</p>
-    <p>Temps restant : <span class="timer" data-fin="{{ enchere.fin }}"></span></p>
-</div>
-{% endfor %}
+<section class="auction-list">
+    <div class="filter-options">
+    </div>
+    <div class="auction-grid">
+        {% for enchere in encheres %}
+        <a href="{{base}}/auction?id={{enchere.enchere_id}}" class="enchere-card">
+            <h2>{{ enchere.titre }}</h2>
+            <img src="{{asset}}uploads/{{ enchere.image_principale }}" alt="{{ enchere.titre }}">
+            <p><strong>Prix départ :</strong> {{ enchere.prix_actuel }} $</p>
+            <p>Temps restant : <span class="timer" data-fin="{{ enchere.fin }}"></span></p>
+        </a>
+        {% endfor %}
+    </div>
+</section>
+
 
 {{ include("layouts/footer.php") }}
