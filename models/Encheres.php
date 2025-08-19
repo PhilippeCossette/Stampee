c00fbd1aaec90d5620ecbae807738bd3d0a55256
Philippe Cossette <philcossette@hotmail.com>
2025-08-19 12:15:36 -0400

    Changed Method in Enchere model

--- models/Encheres.php
+++ models/Encheres.php
@@ -12,34 +12,6 @@ class Encheres extends CRUD
 
     public function getAuctionsWithFilters($filters = [])
     {
-        $where = "WHERE 1=1 ";
-        $params = [];
-
-        if (!empty($filters['color'])) {
-            $where .= "AND t.id_couleur = :color ";
-            $params[':color'] = $filters['color'];
-        }
-
-        if (!empty($filters['condition'])) {
-            $where .= "AND t.id_condition = :condition ";
-            $params[':condition'] = $filters['condition'];
-        }
-
-        if (!empty($filters['year'])) {
-            $where .= "AND t.annee = :year ";
-            $params[':year'] = $filters['year'];
-        }
-
-        if (isset($filters['certified'])) {
-            $where .= "AND t.certifie = :certified ";
-            $params[':certified'] = $filters['certified'];
-        }
-
-        if (!empty($filters['search'])) {
-            $where .= "AND t.titre LIKE :search ";
-            $params[':search'] = '%' . $filters['search'] . '%';
-        }
-
         $sql = "
             SELECT 
                 e.id AS enchere_id,
@@ -62,7 +34,6 @@ class Encheres extends CRUD
                 co.condition AS condition_nom,
 
                 i.url_image AS image_principale,
-
                 COALESCE(MAX(m.montant), e.prix_depart) AS prix_actuel
 
             FROM encheres e
@@ -72,10 +43,38 @@ class Encheres extends CRUD
             LEFT JOIN couleur c ON t.id_couleur = c.id_couleur
             LEFT JOIN `condition` co ON t.id_condition = co.id_condition
             LEFT JOIN mises m ON e.id = m.id_enchere
-            $where
-            GROUP BY e.id
-            ORDER BY e.debut DESC
+            WHERE 1=1
         ";
+
+        $params = [];
+
+        // Filters
+        if (!empty($filters['color'])) {
+            $sql .= " AND t.id_couleur = :color";
+            $params['color'] = $filters['color'];
+        }
+
+        if (!empty($filters['condition'])) {
+            $sql .= " AND t.id_condition = :condition";
+            $params['condition'] = $filters['condition'];
+        }
+
+        if (!empty($filters['year'])) {
+            $sql .= " AND t.annee = :year";
+            $params['year'] = $filters['year'];
+        }
+
+        if (!empty($filters['certified'])) {
+            $sql .= " AND t.certifie = :certified";
+            $params['certified'] = $filters['certified'];
+        }
+
+        if (!empty($filters['search'])) {
+            $sql .= " AND t.titre LIKE :search";
+            $params['search'] = '%' . $filters['search'] . '%';
+        }
+
+        $sql .= " GROUP BY e.id ORDER BY e.debut DESC";
 
         $stmt = $this->prepare($sql);
         $stmt->execute($params);
@@ -84,12 +83,31 @@ class Encheres extends CRUD
 
     public function getFilterOptions()
     {
-        $colors = $this->joinSelection('couleur', 'id_couleur', 'id_couleur', 'couleur', 'ASC', 'color_name');
-        $conditions = $this->joinSelection('condition', 'id_condition', 'id_condition', 'condition', 'ASC', 'condition_name');
+        // Colors
+        $stmt = $this->prepare("
+            SELECT DISTINCT c.id_couleur, c.couleur 
+            FROM timbres t
+            INNER JOIN couleur c ON t.id_couleur = c.id_couleur
+            ORDER BY c.couleur ASC
+        ");
+        $stmt->execute();
+        $colors = $stmt->fetchAll();
 
+        // Conditions
+        $stmt = $this->prepare("
+            SELECT DISTINCT co.id_condition, co.condition 
+            FROM timbres t
+            INNER JOIN `condition` co ON t.id_condition = co.id_condition
+            ORDER BY co.condition ASC
+        ");
+        $stmt->execute();
+        $conditions = $stmt->fetchAll();
+
+        // Years
         $stmt = $this->prepare("SELECT DISTINCT annee FROM timbres ORDER BY annee DESC");
         $stmt->execute();
         $years = $stmt->fetchAll(\PDO::FETCH_COLUMN); // Gives [2024, 2025, 2055] instead of [['annee'=>2024],['annee'=>2025],['annee'=>2055]]
+
 
         return [
             'colors' => $colors,
