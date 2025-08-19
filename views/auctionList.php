c0014bff3f436d8ef7e7487e54bed24f35e381e4
Philippe Cossette <philcossette@hotmail.com>
2025-08-19 11:15:17 -0400

    Ajout de la certification

--- views/auctionList.php
+++ views/auctionList.php
@@ -5,17 +5,28 @@
 <section class="auction-list">
     <div class="filter-options">
     </div>
-    <div class="auction-grid">
+    <div class="auction-list-grid grid">
         {% for enchere in encheres %}
-        <a href="{{base}}/auction?id={{enchere.enchere_id}}" class="enchere-card">
-            <h2>{{ enchere.titre }}</h2>
-            <img src="{{asset}}uploads/{{ enchere.image_principale }}" alt="{{ enchere.titre }}">
-            <p><strong>Prix départ :</strong> {{ enchere.prix_actuel }} $</p>
-            <p>Temps restant : <span class="timer" data-fin="{{ enchere.fin }}"></span></p>
+        <a href="{{base}}/auction?id={{enchere.enchere_id}}" class="auctionShowcase-card">
+            <header class="auctionShowcase-card-header">
+                <h2 class="auctionShowcase-card-header-title">{{ enchere.titre }}</h2>
+                <small class="auctionShowcase-card-small">{{ enchere.condition_nom }}</small>
+                {% if enchere.certifie == 1 %}
+                <img class="auctionShowcase-card-certified" src="{{asset}}img/certified.png" alt="">
+                {% endif %}
+            </header>
+            <picture class="auctionShowcase-card-imgContainer"><img
+                    class="auctionShowcase-card-img"
+                    src="{{asset}}uploads/{{ enchere.image_principale }}"
+                    alt="Image of an auction card displaying stamps" /></picture>
+            <p>{{ enchere.prix_actuel }} $</p>
+            <p><span class="timer" data-fin="{{ enchere.fin }}"></span></p>
+            <button class="button main-button" onclick="window.location.href='{{ base }}/auction?id={{ enchere.enchere_id }}'">Misez</button>
         </a>
         {% endfor %}
     </div>
 </section>
+
 
 
 {{ include("layouts/footer.php") }}
