6887026897a62e18cea76883e19d05390b09be12
Philippe Cossette <philcossette@hotmail.com>
2025-08-25 13:58:01 -0400

    finished function to check if the logged in user is the highest bidder

--- controllers/BidController.php
+++ controllers/BidController.php
@@ -16,9 +16,10 @@ class BidController
     {
         Auth::session();
 
+        $idUser = $_SESSION['user_id'];
         $idEnchere = $_GET['id_enchere'] ?? null;
         if (!$idEnchere) {
-            View::redirect('/auctionlist');
+            View::redirect('auctionlist');
             return;
         }
 
