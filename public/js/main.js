0ae64211b9b7afd1b63f64b369d6f06d4e7033e2
Philippe Cossette <philcossette@hotmail.com>
2025-08-25 17:41:54 -0400

    Finished Zooming Feature

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ import { updateTimers } from "./script/timer.js";
 import { filterMenu } from "./script/filter-menu.js";
 import { imageSlider } from "./script/image-slider.js";
 import { initFavorites } from "./script/favorite.js";
+import { initZoomImages } from "./script/zoom-img.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   if (document.getElementById("delete-btn")) {
@@ -13,6 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
   filterMenu();
   initFavorites();
 
+  if (document.querySelector("[data-zoom='true']")) {
+    initZoomImages();
+  }
+
   if (typeof images !== "undefined") {
     imageSlider(images);
   }
@@ -20,4 +25,3 @@ document.addEventListener("DOMContentLoaded", () => {
   updateTimers();
   setInterval(updateTimers, 1000);
 });
-
