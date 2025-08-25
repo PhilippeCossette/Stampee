c57bf18f8880a8e31acebddb944dddc189660ad6
Philippe Cossette <philcossette@hotmail.com>
2025-08-25 16:08:01 -0400

    Created method to add favorite

--- models/Favoris.php
+++ models/Favoris.php
@@ -7,18 +7,29 @@ use App\Models\CRUD;
 class Favoris extends CRUD
 {
     protected $table = 'favoris';
-    protected $primaryKey = ['id_enchere', 'id_user'];
-    protected $fillable = ['id_enchere', 'id_user'];
+    protected $primaryKey = ['id_utilisateur', 'id_enchere'];
+    protected $fillable = ['id_utilisateur', 'id_enchere'];
 
     public function removeFavorite($idUser, $idEnchere)
     {
         $sql = "
         DELETE 
         FROM {$this->table} 
-        WHERE id_user = :id_user AND id_enchere = :id_enchere
+        WHERE id_utilisateur = :id_utilisateur AND id_enchere = :id_enchere
         ";
         $stmt = $this->prepare($sql);
-        $stmt->bindValue(':id_user', $idUser, \PDO::PARAM_INT);
+        $stmt->bindValue(':id_utilisateur', $idUser, \PDO::PARAM_INT);
+        $stmt->bindValue(':id_enchere', $idEnchere, \PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function addFavorite($idUser, $idEnchere){
+        $sql = "
+        INSERT INTO {$this->table} (id_utilisateur, id_enchere)
+        VALUES (:id_utilisateur, :id_enchere)
+        ";
+        $stmt = $this->prepare($sql);
+        $stmt->bindValue(':id_utilisateur', $idUser, \PDO::PARAM_INT);
         $stmt->bindValue(':id_enchere', $idEnchere, \PDO::PARAM_INT);
         return $stmt->execute();
     }
