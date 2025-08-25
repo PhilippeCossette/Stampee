b175b76296dc17b5932d970d68ce4311c5801688
Philippe Cossette <philcossette@hotmail.com>
2025-08-25 19:03:05 -0400

    Created page to see all favorite auctions

---
+++ views/myFavorites.php
@@ -0,0 +1,46 @@
+{{ include("layouts/header.php", { title: "Mes favoris - Stampee" }) }}
+
+<article class="profile-section">
+    <header class="profile-section-header">
+        <h2 class="profile-section-header-title">Mes enchères favorites</h2>
+        <a class="profile-section-header-link" href="">Voir Tout <i class="fa-solid fa-angle-right"></i></a>
+    </header>
+    <div class="grid">
+        {% if favoris is not empty %}
+        {% for favoris in favoris %}
+        <a href="{{base}}/auction?id={{favoris.enchere_id}}" class="auctionShowcase-card">
+            <header class="auctionShowcase-card-header">
+                {% if favoris.coup_coeur == 1 %}
+                <small class="auctionShowcase-card-header-undertitle">Coups de Coeur</small>
+                {% endif %}
+                <h2 class="auctionShowcase-card-header-title" title="{{ favoris.titre }}">{{ favoris.titre|slice(0, 20) ~ (favoris.titre|length > 20 ? '…' : '') }}</h2>
+                <small class="auctionShowcase-card-small">{{ favoris.condition_nom }}</small>
+                {% if favoris.certifie == 1 %}
+                <img class="auctionShowcase-card-certified" src="{{asset}}img/certified.png" alt="">
+                {% endif %}
+            </header>
+            <picture class="auctionShowcase-card-imgContainer"><img
+                    class="auctionShowcase-card-img"
+                    src="{{asset}}uploads/{{ favoris.image_principale }}"
+                    alt="Image of an auction card displaying stamps" /></picture>
+            <p>{{ favoris.prix_actuel }} $</p>
+            <p>
+                <span class="timer" data-fin="{{ favoris.fin }}"></span>
+            </p>
+        </a>
+        {% endfor %}
+        {% else %}
+        <p class="empty-message">Aucune enchère favorite trouvée.</p>
+        {% endif %}
+    </div>
+</article>
+
+
+
+
+
+
+
+
+
+{{ include("layouts/footer.php") }}
