4d0c11ed9c51e4310aac2318a6aa56ca5a2c60c3
Philippe Cossette <philcossette@hotmail.com>
2025-08-25 14:56:26 -0400

    created js function for handling click on fav buttons also imported it in the main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,14 @@ import { initConfirm } from "./script/confirm-delete.js";
 import { updateTimers } from "./script/timer.js";
 import { filterMenu } from "./script/filter-menu.js";
 import { imageSlider } from "./script/image-slider.js";
+import {initFavorites} from "./script/favorite.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initConfirm("delete-btn", "/Stampee/user/delete");
   initNav();
   filterMenu();
+  initFavorites();
+
   imageSlider(images);
 
   updateTimers();
