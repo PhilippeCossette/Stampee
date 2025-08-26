7dad5b415659ee6a2426800b41442cf531a7f234
Philippe Cossette <philcossette@hotmail.com>
2025-08-26 13:16:21 -0400

    Created new model Commentaire

---
+++ models/Commentaire.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\CRUD;
+
+class Commentaire extends CRUD
+{
+    protected $table = 'commentaire';
+    protected $primaryKey = 'id';
+    protected $fillable = ['contenu', 'date_heure', 'id_utilisateur', 'id_enchere'];
+
+    
+}
