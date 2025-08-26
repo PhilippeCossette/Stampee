e07e86fa01bf03fdd9707f1203a47ae262687f85
Philippe Cossette <philcossette@hotmail.com>
2025-08-26 13:21:51 -0400

    Create method in commentaire model to fetch all comments of an auction

--- models/Commentaire.php
+++ models/Commentaire.php
@@ -10,5 +10,28 @@ class Commentaire extends CRUD
     protected $primaryKey = 'id';
     protected $fillable = ['contenu', 'date_heure', 'id_utilisateur', 'id_enchere'];
 
-    
+    public function showCommentaire($id)
+    {
+        $sql = "
+            SELECT 
+                c.id AS commentaire_id,
+                c.contenu,
+                c.date_heure,
+
+                u.id AS utilisateur_id,
+                u.nom_utilisateur,
+                u.email
+                
+                FROM {$this->table} c
+                INNER JOIN utilisateur u ON c.id_utilisateur = u.id
+                WHERE c.id_enchere = :enchere_id
+                ORDER BY c.date_heure ASC
+        ";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':enchere_id', $id, \PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
