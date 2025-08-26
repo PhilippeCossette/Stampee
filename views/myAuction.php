5bbddaddcf8f38825f8ec91a9586c79aa9a5c7fa
Philippe Cossette <philcossette@hotmail.com>
2025-08-25 23:56:04 -0400

    Created page myAuction.php and made the twig structure to display ALL of my auctions

--- views/myAuction.php
+++ views/myAuction.php
@@ -1,31 +1,31 @@
-{{ include("layouts/header.php", { title: "Mes favoris - Stampee" }) }}
+{{ include("layouts/header.php", { title: "Mes Encheres - Stampee" }) }}
 <section class="max-1200 inlineP-huge">
     <article class="profile-section">
         <header class="profile-section-header">
-            <h2 class="profile-section-header-title">Mes enchères favorites</h2>
+            <h2 class="profile-section-header-title">Mes enchères</h2>
             <a class="profile-section-header-link" href="{{ base }}/profile">Mon Profile <i class="fa-solid fa-angle-right"></i></a>
         </header>
         <div class="grid">
-            {% if favoris is not empty %}
-            {% for favoris in favoris %}
-            <a href="{{base}}/auction?id={{favoris.enchere_id}}" class="auctionShowcase-card">
+            {% if mesEnchere is not empty %}
+            {% for mesEnchere in mesEnchere %}
+            <a href="{{base}}/auction?id={{mesEnchere.enchere_id}}" class="auctionShowcase-card">
                 <header class="auctionShowcase-card-header">
-                    {% if favoris.coup_coeur == 1 %}
+                    {% if mesEnchere.coup_coeur == 1 %}
                     <small class="auctionShowcase-card-header-undertitle">Coups de Coeur</small>
                     {% endif %}
-                    <h2 class="auctionShowcase-card-header-title" title="{{ favoris.titre }}">{{ favoris.titre|slice(0, 20) ~ (favoris.titre|length > 20 ? '…' : '') }}</h2>
-                    <small class="auctionShowcase-card-small">{{ favoris.condition_nom }}</small>
-                    {% if favoris.certifie == 1 %}
+                    <h2 class="auctionShowcase-card-header-title" title="{{ mesEnchere.titre }}">{{ mesEnchere.titre|slice(0, 20) ~ (mesEnchere.titre|length > 20 ? '…' : '') }}</h2>
+                    <small class="auctionShowcase-card-small">{{ mesEnchere.condition_nom }}</small>
+                    {% if mesEnchere.certifie == 1 %}
                     <img class="auctionShowcase-card-certified" src="{{asset}}img/certified.png" alt="">
                     {% endif %}
                 </header>
                 <picture class="auctionShowcase-card-imgContainer"><img
                         class="auctionShowcase-card-img"
-                        src="{{asset}}uploads/{{ favoris.image_principale }}"
+                        src="{{asset}}uploads/{{ mesEnchere.image_principale }}"
                         alt="Image of an auction card displaying stamps" /></picture>
-                <p>{{ favoris.prix_actuel }} $</p>
+                <p>{{ mesEnchere.prix_actuel }} $</p>
                 <p>
-                    <span class="timer" data-fin="{{ favoris.fin }}"></span>
+                    <span class="timer" data-fin="{{ mesEnchere.fin }}"></span>
                 </p>
             </a>
             {% endfor %}
